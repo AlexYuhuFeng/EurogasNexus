38bc0145564d2b876741a2b1604c0fcb7608e12e
Fluffy Dog <qqshuyy@gmail.com>
2026-07-06 23:14:25 +0800

    Add App builder export barrel

---
+++ clients/web/src/app/index.ts
@@ -0,0 +1,14 @@
+export { buildContractPayload } from "./contractPayload";
+export { cloneDefaultContractDraft, defaultContractDraft } from "./defaultContractDraft";
+export type { ContractDraft } from "./defaultContractDraft";
+export { contractDraftFromRecord, contractRecordFromImportedFile } from "./contractImport";
+export { buildResourcePoolOptimizationRequest } from "./resourcePoolRequest";
+export { buildRouteRecommendationRequest } from "./routeRecommendationRequest";
+export { buildStrategyScenario } from "./strategyScenario";
+export {
+  metadataText,
+  normalizePointName,
+  routeEdgeMetadataText,
+  routeEdgeRouteId,
+  routeLegLabel,
+} from "./routeMetadata";
