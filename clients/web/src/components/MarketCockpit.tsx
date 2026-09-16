284ad1e1adb5cbaa7b987bf335abdddeacbb87b4
Fluffy Dog <qqshuyy@gmail.com>
2026-09-16 14:55:42 +0800

    refactor(market): stop duplicating network context props

--- clients/web/src/components/MarketCockpit.tsx
+++ clients/web/src/components/MarketCockpit.tsx
@@ -339,12 +339,8 @@ export function MarketCockpit({ controller }: { controller: AppController }) {
             strategyResult={api.strategyResult}
             activeWarning={portfolio.activeWarning}
             reviewEvidenceItems={portfolio.reviewEvidenceItems}
-            gasDay={traderContext.gasDay}
-            deliveryProduct={traderContext.deliveryProduct}
-            hubId={traderContext.hubId}
             marketLastUpdatedAtUtc={api.marketLastUpdatedAtUtc}
             intradayOpportunities={api.intradayOpportunities}
-            sourceStats={controller.sources.sourceStats}
             optimizerContextMismatch={portfolio.optimizerContextMismatch}
             onResetSearch={() => controls.setSearchTerm("")}
             onToggleLayer={controls.toggleLayer}
